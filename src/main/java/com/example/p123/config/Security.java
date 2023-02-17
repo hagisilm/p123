5a3727f9f8c0bb468a629c46cecdee7cb02b3831
hagisilm <catchwall3203@naver.com>
2023-02-17 21:10:42 +0900

    master2

--- src/main/java/com/example/p123/config/Security.java
+++ src/main/java/com/example/p123/config/Security.java
@@ -1,12 +1,36 @@
 package com.example.p123.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class Security {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        try {
+            return http.csrf().disable()
+                    .authorizeRequests()
+                    .mvcMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest()
+                    .permitAll()
+                    .and()
+                    .formLogin()
+                    .loginPage("/users/login")
+                    .loginProcessingUrl("/login")
+                    .defaultSuccessUrl("/")
+                    .failureUrl("/users/login/error")
+                    .and().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
